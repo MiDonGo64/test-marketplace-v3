3847c837d423a28910a3fac2ce08661e1d7bd7a6
MiDonGo64 <miles.donald83@gmail.com>
2023-05-18 07:42:32 -0500

    test

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
   images: {
     domains: ["gateway.ipfscdn.io", "i.seadn.io", "ipfs.io"],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
